2ea9366fabb6ac69b3bc996743a7fce5d334d920
Jean-Peer Lorenz <peer.loz@gmx.net>
2021-04-16 20:22:10 +0200

    Update versions and dependencies
    Kotlin, Gradle and library dependencies are updated to latest.
    
    FIX: use update method to get navController in MainActivity.

--- app/src/main/java/com/udacity/locationreminder/locationreminders/RemindersActivity.kt
+++ app/src/main/java/com/udacity/locationreminder/locationreminders/RemindersActivity.kt
@@ -3,25 +3,28 @@ package com.udacity.locationreminder.locationreminders
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.findNavController
 import com.udacity.locationreminder.R
-import kotlinx.android.synthetic.main.activity_reminders.*
+import com.udacity.locationreminder.databinding.ActivityRemindersBinding
 
 /**
  * The RemindersActivity that holds the reminders fragments
  */
 class RemindersActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityRemindersBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reminders)
+        binding = ActivityRemindersBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
-                (nav_host_fragment as NavHostFragment).navController.popBackStack()
+                findNavController(R.id.nav_host_fragment).popBackStack()
                 return true
             }
         }
