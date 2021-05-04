e6a9591693e4dacb43452e5ea81594c523b0e52f
Jean-Peer Lorenz <peer.loz@gmx.net>
2021-05-04 21:04:20 +0200

    TEST: implement instrumented End-to-End test

--- app/src/androidTest/java/com/udacity/locationreminder/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/locationreminder/RemindersActivityTest.kt
@@ -1,16 +1,31 @@
 package com.udacity.locationreminder
 
 import android.app.Application
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.udacity.locationreminder.locationreminders.RemindersActivity
 import com.udacity.locationreminder.locationreminders.data.ReminderDataSource
+import com.udacity.locationreminder.locationreminders.data.dto.ReminderDTO
 import com.udacity.locationreminder.locationreminders.data.local.LocalDB
 import com.udacity.locationreminder.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.locationreminder.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.locationreminder.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.locationreminder.util.DataBindingIdlingResource
+import com.udacity.locationreminder.util.EspressoIdlingResource
+import com.udacity.locationreminder.util.monitorActivity
 import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -49,7 +64,8 @@ class RemindersActivityTest :
                     get() as ReminderDataSource
                 )
             }
-            single { RemindersLocalRepository(get()) }
+            @Suppress("USELESS_CAST")
+            single { RemindersLocalRepository(get()) as ReminderDataSource }
             single { LocalDB.createRemindersDao(appContext) }
         }
         //declare a new koin module
@@ -65,7 +81,119 @@ class RemindersActivityTest :
         }
     }
 
+    // End to End testing to the app
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
 
-//    TODO: add End to End testing to the app
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
 
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
+
+    private val reminders = listOf(
+        ReminderDTO(
+            title = "Title of reminder 1",
+            description = "Description of reminder 1",
+            location = "User defined location",
+            latitude = 10.0,
+            longitude = 10.0
+        ),
+        ReminderDTO(
+            title = "Title of reminder 2",
+            description = "Description of reminder 2",
+            location = "User defined location",
+            latitude = 20.0,
+            longitude = 20.0
+        ),
+    )
+
+    @Test
+    fun addNewReminder_navigatesToSaveReminderFragmentAndItemIsDisplayedInRemindersList() =
+        runBlocking {
+            repository.saveReminder(reminders[0])
+            repository.saveReminder(reminders[1])
+
+            val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+            dataBindingIdlingResource.monitorActivity(activityScenario)
+
+            onView(withId(R.id.addReminderFAB)).perform(click())
+
+            onView(withId(R.id.reminderTitle)).perform(replaceText("Title of reminder 3"))
+            onView(withId(R.id.reminderDescription)).perform(replaceText("Description of reminder 3"))
+
+            onView(withId(R.id.selectLocation)).perform(click())
+            onView(withId(R.id.map)).check(matches(isDisplayed()))
+            onView(withId(R.id.map)).perform(longClick())
+            onView(withId(R.id.saveLocation)).perform(click())
+
+            onView(withId(R.id.saveReminder)).perform(click())
+
+            onView(withId(R.id.reminderssRecyclerView))
+                .check(matches(hasDescendant(withText("Title of reminder 1"))))
+            onView(withId(R.id.reminderssRecyclerView))
+                .check(matches(hasDescendant(withText("Title of reminder 2"))))
+            onView(withId(R.id.reminderssRecyclerView))
+                .check(matches(hasDescendant(withText("Title of reminder 3"))))
+
+            activityScenario.close()
+        }
+
+    @Test
+    fun addReminder_noLocationIsSelected_errorMessageIsDisplayed(): Unit = runBlocking {
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderTitle)).perform(replaceText("Title of reminder"))
+        onView(withId(R.id.reminderDescription)).perform(replaceText("Description of reminder"))
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        val snackbarMessage = appContext.getString(R.string.select_location)
+        onView(withText(snackbarMessage)).check(matches(isDisplayed()))
+        activityScenario.close()
+    }
+
+    @Test
+    fun clickReminderListItem_remindersDescriptionIsDisplayed(): Unit = runBlocking {
+        repository.saveReminder(reminders[0])
+        repository.saveReminder(reminders[1])
+
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        onView(withId(R.id.reminderssRecyclerView))
+            .check(matches(hasDescendant(withText("Title of reminder 1"))))
+            .check(matches(hasDescendant(withText("Title of reminder 2"))))
+
+        val position = 0
+        onView(withId(R.id.reminderssRecyclerView)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                position,
+                clickOnViewChild(R.id.reminderCardView)
+            )
+        )
+
+        onView(withId(R.id.reminderTitle))
+            .check(matches(isDisplayed()))
+            .check(matches(withText("Title of reminder 1")))
+
+        onView(withId(R.id.reminderDescription))
+            .check(matches(isDisplayed()))
+            .check(matches(withText("Description of reminder 1")))
+
+        onView(withId(R.id.reminderLocation))
+            .check(matches(isDisplayed()))
+            .check(matches(withText("User defined location")))
+
+        onView(withId(R.id.reminderLatLng))
+            .check(matches(isDisplayed()))
+
+        activityScenario.close()
+    }
 }
